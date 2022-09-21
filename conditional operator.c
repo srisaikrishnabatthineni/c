3a9d6f710ac10244f0750633463acdd30643c64f
srisaikrishnabatthineni <105336579+srisaikrishnabatthineni@users.noreply.github.com>
2022-09-21 18:52:50 +0530

    Add files via upload

---
+++ conditional operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+//basic code
+  int input;//take a value and stored in input variable
+  printf("Enter a number: ");
+  scanf("%d",&input);
+//condition
+  (input%2==0)?printf("Even number"):printf("odd number");//conditional operator code
+
+
+
+
+}
